eaf622050d9f80c43f3753f63b7e75657c90fbcc
mqamrudd <mqamrudd@BNGLT-DDCZ1Z2.global.sial.com>
2022-01-19 12:09:42 +0530

    Second commit

--- src/main/java/MerckFristDemo.java
+++ src/main/java/MerckFristDemo.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
-
+ 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
- 
 public class MerckFristDemo extends HttpServlet {
 
 	/**
@@ -21,7 +21,7 @@ public class MerckFristDemo extends HttpServlet {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
-        context.addServlet(new ServletHolder(new MerckFirstDemo()), "/*");
+        context.addServlet(new ServletHolder(new MerckFristDemo()), "/*");
         server.start();
         server.join();
 	}
